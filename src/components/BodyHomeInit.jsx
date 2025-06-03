22299e5ca93935e3c8c90d0415c307825353f8f2
raamonfr <ramonfranca2012@gmail.com>
2025-06-03 01:30:53 -0300

    rotas ajustadas

--- src/components/BodyHomeInit.jsx
+++ src/components/BodyHomeInit.jsx
@@ -4,30 +4,29 @@ import { Link } from 'react-router-dom';
 
 function BodyHomeInit() {
   return (
-    <div id='bodyHomeInit' className="w-screen flex justify-center items-center">
-      <div id='contentTextBodyInit' className="flex flex-col gap-6 max-lg:items-center max-lg:text-center m-5 max-sm:my-20 md:p-40 lg:p-20 xl:p-40 lg:w-1/2 lg:my-20">
-        <h1 className='font-bold text-zinc-300'>Bem-vindo ao Millenium</h1>
+    <div className="w-full flex justify-between mt-56 mb-20 px-40">
+      <div className="w-1/2 max-w-[700px] space-y-5">
+        <h1 className="text-8xl font-bold text-zinc-300">
+          Bem-vindo ao Millenium
+        </h1>
 
         <p className="font-light text-zinc-400">
-          Impulsione seu negócio com tecnologia inovadora! Nossa plataforma segura, eficiente e escalável foi desenvolvida com o que há de mais avançado para oferecer a melhor experiência. 🚀
+          Uma plataforma segura, eficiente e escalável, desenvolvida com
+          tecnologia de ponta para garantir a melhor experiência. 🚀
         </p>
 
         <p>Ainda não faz parte dessa equipe?</p>
 
-        <Link to="/cadastro" className="text-white max-w-[270px] rounded-lg border-2 border-transparent flex gap-4 justify-center items-center text-2xl bg-blue-500 font-bold p-3 rounded-lg transition duration-500 ease-in-out  hover:border-current hover:text-white">
+        <Link to="/cadastro" className="text-white max-w-[270px] rounded-lg border-2 border-transparent flex gap-4 justify-center items-center text-2xl bg-blue-500 font-bold p-3 transition duration-500 ease-in-out  hover:border-current hover:text-white">
           Cadastre-se <ChevronRightIcon />
         </Link>
       </div>
 
-      
-      <figure className='w-1/2 flex justify-center m-5 p-20 items-center max-lg:w-full max-lg:hidden'>
-        <img 
-          id='contentImageBodyInit' 
-          src={ImagemHome} 
-          alt="Imagem aleatória" 
-          className="w-full h-auto xl:w-[500px]"
-        />
-      </figure>
+      <img
+        src={ImagemHome}
+        alt="Imagem aleatoria"
+        className="w-1/2 max-w-[500px]"
+      />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,98 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Cadastro from './pages/Cadastro.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './pages/ErroPage.jsx'
-import Login from './pages/Login.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+import App from './App.jsx';
+import Cadastro from './pages/CadastroEmpresa.jsx';
+import Login from './pages/LoginEmpresa.jsx';
+import Estoque from './pages/Estoque.jsx';
+import ErrorPage from './pages/ErroPage.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx'; // 🛡️ Importa o PrivateRoute
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CadastroONG from './pages/CadastroONG.jsx';
+import TelaEscolhaLogin from './pages/TelaEscolhaLogin.jsx';
+import TelaEscolhaCadastro from './pages/TelaEscolhaCadastro.jsx';
+import CadastroEmpresa from './pages/CadastroEmpresa.jsx';
+import LoginEmpresa from './pages/LoginEmpresa.jsx';
+import LoginOng from './pages/LoginOng.jsx';
+import Vendas from './pages/Vendas.jsx';
+import Doacoes from './pages/Doacoes.jsx';
+import DoacoesOng from "./pages/DoacoesOng.jsx";
+import Compras from "./pages/Compras"; // ✅ certo
+import MeusProdutos from './pages/Dashboard/MeusProdutos.jsx';
+import DashboardLayout from './layouts/DashboardLayout.jsx'
+import PagiInicial from './pages/Dashboard/PagInicial.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
     errorElement: <ErrorPage />
   },
   {
     path: "/cadastro",
-    element: <Cadastro />,
+    element: <TelaEscolhaCadastro />,
   },
   {
     path: "/login",
-    element: <Login />
-  }
-])
+    element: <TelaEscolhaLogin />
+  },
+  {
+    path: "/cadastroOng",
+    element: <CadastroONG />
+  },
+  {
+    path: "/cadastroEmpresa",
+    element: <CadastroEmpresa />
+  },
+  {
+    path: "/loginEmpresa",
+    element: <LoginEmpresa />
+  },
+  {
+    path: "loginOng",
+    element: <LoginOng />
+  },
+  {
+    path: "/vendas",
+    element: <Vendas />
+  },
+  {
+    path: "/doacoes-ong",
+    element: <DoacoesOng />
+  },
+  {
+    path: "/compras",
+    element: <Compras />
+  },
+  {
+    path: "/estoque",
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true, // /paginainicial
+        element: <PagiInicial />,
+      },
+      {
+        path: "meusprodutos", // /estoque/meusprodutos
+        element: <MeusProdutos />,
+      },
+      {
+        path: "doacoes",
+        element: <Doacoes />
+      }
+      // você pode adicionar mais páginas do dashboard aqui
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
